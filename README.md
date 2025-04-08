b668a4e877171f132562c823df9dbe06a5513701
Maksim Titov <mstitov.mail@gmail.com>
2025-04-08 13:17:16 +0300

    init commint

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# Проектная работа Mesto
-
+https://github.com/mstitovgit/mesto-project-ff
--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,18 @@
-// @todo: Темплейт карточки
+const cardTemplate = document.querySelector("#card-template").content;
+const cardNode = cardTemplate.querySelector(".card");
+const placesList = document.querySelector(".places__list");
 
-// @todo: DOM узлы
+initialCards.forEach((card) => placesList.append(createCard(card, removeCard)));
 
-// @todo: Функция создания карточки
+function createCard(card, removeCard) {
+  const newCard = cardNode.cloneNode(true);
+  newCard.querySelector(".card__image").src = card.link;
+  newCard.querySelector(".card__image").alt = card.name;
+  newCard.querySelector(".card__title").textContent = card.name;
+  newCard.querySelector(".card__delete-button").addEventListener("click", () => removeCard(newCard));
+  return newCard;
+}
 
-// @todo: Функция удаления карточки
-
-// @todo: Вывести карточки на страницу
+function removeCard(card) {
+  card.remove();
+}
